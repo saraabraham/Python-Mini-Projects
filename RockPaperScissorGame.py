daf91501ee5a8bb312df1cd3a55b4681f6309a9c
sara <sara.abraham12@gmail.com>
2025-03-14 09:43:24 +0100

    Added recursive function and emojis

--- RockPaperScissorGame.py
+++ RockPaperScissorGame.py
@@ -6,46 +6,51 @@ class game(Enum):
     ROCK=1
     PAPER=2
     SCISSORS=3
-playagain = True
 
-while playagain:
-
-    print(" ")
-    print("Rock Paper Scissors Game")
-    print("")
-    playerChoice = input("Enter... \n1 for Rock \n2 for Paper \n3 for Scissors\n\n")
+def rpsgame():
+    print("\nRock Paper Scissors Game 🪨 🗞️ ✂️")
+    playerChoice = input("\nEnter... \n1 for Rock  🪨 \n2 for Paper  🗞️ \n3 for Scissors ✂️ \n\n")
+    if playerChoice not in ["1","2","3"]:
+        print("Invalid input: Please enter 1,2,3")
+        return rpsgame()
+    
     player = int(playerChoice)
-
-    if player < 1 or player > 3:
-        sys.exit("Invalid Input")
-
     computerChoice = random.choice("123")
     computer=int(computerChoice)
 
-    print("\nYou chose the weapon " + str(game(player)).replace("game.","")+".")
-    print("Python chose the weapon " + str(game(computer)).replace("game.","")+".")   
+    print("\nYou chose the weapon " + str(game(player)).replace("game.","").lower()+".")
+    print("Python chose the weapon " + str(game(computer)).replace("game.","").lower()+".")   
 
     if player == 1 and computer == 3:
-        print("\nYou Win! :)")
+     print("\nYou Win! 👻")
 
     elif player == 2 and computer == 1:
-        print("\nYou Win! :)")
+     print("\nYou Win! 👻")
 
     elif player == 3 and computer == 2:
-        print("\nYou Win! :)")
+     print("\nYou Win! 👻")
     elif player == computer :
-        print("\nIt\'s a Tie! :|")
+     print("\nIt\'s a Tie! 😥")
     else:
-        print("\nPython wins!! :(")
-    playagain = input("\nPlay again? \n1.Enter Y for Yes \n2.Enter Q for Quit\n\n")
+     print("\nPython wins!! 🐍")
+    
+    print("\nPlay again?🤟")
+          
+    while True:
+        playagain = input("\n1.Enter Y for Yes \n2.Enter Q for Quit\n\n")
 
+        if playagain.lower() not in ['y','q']:
+            continue
+        else:
+           break
+    
     if playagain.lower() == 'y':
-        playagain = True
+            return rpsgame()
     else:
-        playagain = False
-        sys.exit("\nBye!!\n")
+            print("\nThankyou for playing!🤗")
+            sys.exit("\nBye!!👋\n")
 
-
+rpsgame()
 
 
 
