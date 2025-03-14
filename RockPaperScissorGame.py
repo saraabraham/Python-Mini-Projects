eefe2f8da12fc7e1a19706503d9da54b6fd9866f
sara <sara.abraham12@gmail.com>
2025-03-14 13:06:12 +0100

    added closure and wins count

--- RockPaperScissorGame.py
+++ RockPaperScissorGame.py
@@ -2,64 +2,86 @@ import sys
 import random
 from enum import Enum
 
-class game(Enum):
-    ROCK=1
-    PAPER=2
-    SCISSORS=3
-game_count = 1
-def rpsgame():
-    print("\nRock Paper Scissors Game 🪨 🗞️ ✂️")
-    playerChoice = input("\nEnter... \n1 for Rock  🪨 \n2 for Paper  🗞️ \n3 for Scissors ✂️ \n\n")
-    if playerChoice not in ["1","2","3"]:
-        print("Invalid input: Please enter 1,2,3")
-        return rpsgame()
+def rps():
+    game_count = 1
+    player_wins = 0
+    python_wins=0
     
-    player = int(playerChoice)
-    computerChoice = random.choice("123")
-    computer=int(computerChoice)
-
-    print("\nYou chose the weapon " + str(game(player)).replace("game.","").lower()+".")
-    print("Python chose the weapon " + str(game(computer)).replace("game.","").lower()+".")  
-
-    def winner_decide(player,computer): 
-
-        if player == 1 and computer == 3:
-            return "\nYou Win! 👻"
-
-        elif player == 2 and computer == 1:
-            return "\nYou Win! 👻" 
-
-        elif player == 3 and computer == 2:
-            return "\nYou Win! 👻" 
-        elif player == computer :
-            return "\nIt\'s a Tie! 😥"
-        else:
-            return "\nPython wins!! 🐍"
     
-    winner_text=winner_decide(player,computer)
-    print(winner_text)
-    global game_count 
-    game_count +=1
 
-    
-    print("\nPlay again?🤟")
-          
-    while True:
-        playagain = input("\n1.Enter Y for Yes \n2.Enter Q for Quit\n\n")
+    def rpsgame():
+        nonlocal player_wins
+        nonlocal python_wins
 
-        if playagain.lower() not in ['y','q']:
-            continue
-        else:
-           break
-    
-    if playagain.lower() == 'y':
-            print("This is game No:"+ str(game_count))
+        class game(Enum):
+            ROCK=1
+            PAPER=2
+            SCISSORS=3 
+
+        print("\nRock Paper Scissors Game 🪨 🗞️ ✂️")
+        playerChoice = input("\nEnter... \n1 for Rock  🪨 \n2 for Paper  🗞️ \n3 for Scissors ✂️ \n\n")
+        if playerChoice not in ["1","2","3"]:
+            print("\nInvalid input: Please enter 1,2,3")
             return rpsgame()
-    else:
-            print("\nThankyou for playing!🤗")
-            sys.exit("\nBye!!👋\n")
+        
+        player = int(playerChoice)
+        computerChoice = random.choice("123")
+        computer=int(computerChoice)
 
-rpsgame()
+        print("\nYou chose the weapon " + str(game(player)).replace("game.","").lower()+".")
+        print("Python chose the weapon " + str(game(computer)).replace("game.","").lower()+".")  
+
+        def winner_decide(player,computer): 
+            nonlocal player_wins
+            nonlocal python_wins
+
+            if player == 1 and computer == 3:
+                player_wins += 1
+                return "\nYou Win! 👻"
+
+            elif player == 2 and computer == 1:
+                player_wins += 1
+                return "\nYou Win! 👻" 
+
+            elif player == 3 and computer == 2:
+                player_wins += 1
+                return "\nYou Win! 👻" 
+            elif player == computer :
+                return "\nIt\'s a Tie! 😥"
+            else:
+                python_wins += 1
+                return "\nPython wins!! 🐍"
+        
+        winner_text=winner_decide(player,computer)
+        print(winner_text)
+        print("\nPlayer wins:" + str(player_wins))
+        print("\nPython wins:" + str(python_wins))     
+        nonlocal game_count 
+        game_count +=1
+
+        
+        print("\nPlay again?🤟")
+            
+        while True:
+            playagain = input("\n1.Enter Y for Yes \n2.Enter Q for Quit\n\n")
+
+            if playagain.lower() not in ['y','q']:
+                continue
+            else:
+             break
+        
+        if playagain.lower() == 'y':
+                print("\nThis is game No:"+ str(game_count))
+                return rpsgame()
+        else:
+                print("\nThankyou for playing!🤗")
+                sys.exit("\nBye!!👋\n")
+    return rpsgame
+
+play = rps()
+play()
+     
+    
 
 
 
